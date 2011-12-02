483240fa428976e7ed3fc3b94c398aeb894b2dbd
Olga Rybacka <Olga.Rybacka@gmail.com>
2011-12-02 20:52:17 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Megi::Application.config.secret_token = '1142411bda89b1335cca5abfce2eaff0c97a6af6ad6d4dee64870ca15722b486399cb56583825bf9e3a5b35f6e8edf458f2a2a59a9ea6ac51042302a21c871ac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Megi::Application.config.session_store :cookie_store, key: '_megi_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Megi::Application.config.session_store :active_record_store
